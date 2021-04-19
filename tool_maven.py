3fa296fa445331edf56c5dab67aaa78c85748ebd
Marcello Cordeiro <msc3@cin.ufpe.br>
2021-04-19 00:05:51 -0300

    Exit on compilation error

--- tool_maven.py
+++ tool_maven.py
@@ -1,3 +1,4 @@
+import logging
 from shutil import copy
 
 from base_tool import BaseTool
@@ -6,8 +7,12 @@ from util import subprocess_run
 
 class Maven(BaseTool):
     def setup(self):
-        command = "mvn package -q"
-        subprocess_run(command, cwd=str(self.directory))
+        command = "mvn compile -q"
+        result = subprocess_run(command, cwd=str(self.directory)).returncode
+        
+        if result != 0:
+            logging.error(f"Compilation returned with error code {result}")
+            exit(result)
 
     def run_tests(self, report_folder):
         command = f"mvn test {self.arguments} -q"
